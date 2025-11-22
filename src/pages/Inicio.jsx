faec45f4f6b4c4bf9a6d0c4db210bd3c19c69c16
CARLOS ÉVERTON ALVES MANGUEIRA <CARLOSEVERTON1995@GMAIL.COM>
2025-11-22 13:20:07 -0300

    1

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,11 +6,11 @@ import "../css/index.css";
 import Carousel from "../components/Carousel";
 
 // BANCO DE DADOS LOCAL
-import inicioData from "../data/inicioData.js"; // Importa como InicioData
+import InicioData from "../data/inicioData.js"; // Importa como InicioData
 
 export default function Inicio() {
   // CORREÇÃO: Use InicioData (com I maiúsculo) em vez de inicioData
-  const { edicaoAtual, edicoesAnteriores, carrossel } = inicioData;
+  const { edicaoAtual, edicoesAnteriores, carrossel } = InicioData;
 
   return (
     <>
